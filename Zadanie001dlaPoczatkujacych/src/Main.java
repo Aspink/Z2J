e9b8fd6054b41691a787b8e72f260c4ccac6c2df
Aspink <aspink1@wp.pl>
2022-11-23 21:59:04 +0100

    Dodano kolejny wydruk

--- Zadanie001dlaPoczatkujacych/src/Main.java
+++ Zadanie001dlaPoczatkujacych/src/Main.java
@@ -2,8 +2,18 @@ public class Main {
     public static void main(String[] args) {
         String message = "Zero To Junior";
         int length = message.length();
+        System.out.println(message  + "/n");
+        System.out.println("Cztery pierwsze znaki: " + SubString.subString(message, 0, 4));
+        System.out.println("Pięć ostatnich znaków: " + SubString.subString(message, length-5, length));
+        int whereToIs = 0;
+        String text;
+        do {
+            text = SubString.subString(message, whereToIs, whereToIs+2);
+            System.out.println(text + " " + whereToIs);
+            whereToIs++;
+        }
+        while(!text.equals("To"));
+        System.out.println("Od \"To\" do końca: " + SubString.subString(message, whereToIs-1, length));
 
-        System.out.println(SubString.subString(message, 0, 4));
-        System.out.println(SubString.subString(message, length-5, length));
     }
 }
