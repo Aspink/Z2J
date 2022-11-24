53e6b46be21d2406a84d7ed44e843bffc03f7dc1
Aspink <aspink1@wp.pl>
2022-11-24 14:34:49 +0100

    Programwykonuje całe zadanie - This is the end.

--- Zadanie001dlaPoczatkujacych/src/Main.java
+++ Zadanie001dlaPoczatkujacych/src/Main.java
@@ -2,18 +2,17 @@ public class Main {
     public static void main(String[] args) {
         String message = "Zero To Junior";
         int length = message.length();
-        System.out.println(message  + "/n");
+        System.out.println(message  + "\n");
         System.out.println("Cztery pierwsze znaki: " + SubString.subString(message, 0, 4));
         System.out.println("Pięć ostatnich znaków: " + SubString.subString(message, length-5, length));
         int whereToIs = 0;
         String text;
         do {
             text = SubString.subString(message, whereToIs, whereToIs+2);
-            System.out.println(text + " " + whereToIs);
             whereToIs++;
         }
         while(!text.equals("To"));
         System.out.println("Od \"To\" do końca: " + SubString.subString(message, whereToIs-1, length));
-
+        System.out.println("Znaki od 3 do 8: " + SubString.subString(message, 3-1, 8));
     }
 }
