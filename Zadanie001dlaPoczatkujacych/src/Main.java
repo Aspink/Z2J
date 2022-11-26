43dfaed96e5cefca68b81e7a4e4c18a89d9c82af
Aspink <aspink1@wp.pl>
2022-11-26 20:37:17 +0100

    Przeniesiono metodę subString do klasy Main i usunięto klasę SubString

--- Zadanie001dlaPoczatkujacych/src/Main.java
+++ Zadanie001dlaPoczatkujacych/src/Main.java
@@ -3,16 +3,24 @@ public class Main {
         String message = "Zero To Junior";
         int length = message.length();
         System.out.println(message  + "\n");
-        System.out.println("Cztery pierwsze znaki: " + SubString.subString(message, 0, 4));
-        System.out.println("Pięć ostatnich znaków: " + SubString.subString(message, length-5, length));
+        System.out.println("Cztery pierwsze znaki: " + subString(message, 0, 4));
+        System.out.println("Pięć ostatnich znaków: " + subString(message, length-5, length));
         int whereToIs = 0;
         String text;
         do {
-            text = SubString.subString(message, whereToIs, whereToIs+2);
+            text = subString(message, whereToIs, whereToIs+2);
             whereToIs++;
         }
         while(!text.equals("To"));
-        System.out.println("Od \"To\" do końca: " + SubString.subString(message, whereToIs-1, length));
-        System.out.println("Znaki od 3 do 8: " + SubString.subString(message, 3-1, 8));
+        System.out.println("Od \"To\" do końca: " + subString(message, whereToIs-1, length));
+        System.out.println("Znaki od 3 do 8: " + subString(message, 3-1, 8));
+    }
+
+    public static String subString(String text, int start, int end) {
+        //metoda zastępująca metodę "substring" klasy String
+        char[] textArray = text.toCharArray();
+        String textSub = "";
+        for(int i = 0; i < end - start; i++) textSub = textSub + textArray[i + start];
+        return textSub;
     }
 }
--- Zadanie001dlaPoczatkujacych/src/SubString.java
+++
@@ -1,8 +0,0 @@
-public class SubString {
-    public static String subString(String text, int start, int end) {
-        char[] textArray = text.toCharArray();
-        String textSub = "";
-        for(int i = 0; i < end - start; i++) textSub = textSub + textArray[i + start];
-        return textSub;
-    }
-}
