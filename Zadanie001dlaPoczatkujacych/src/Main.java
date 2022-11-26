9126028d407af79c9d6355ff0d130229b60b47f3
Aspink <aspink1@wp.pl>
2022-11-26 21:08:55 +0100

    PrzUtworzono metodę whereToIs i przeniesiono do niej odpowiz metody mainedzialność za znalezienie pozycji określnego ącigu znaków w przekazanym tekście

--- Zadanie001dlaPoczatkujacych/src/Main.java
+++ Zadanie001dlaPoczatkujacych/src/Main.java
@@ -1,18 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        String message = "Zero To Junior";
-        int length = message.length();
+        String message = "Zero To Junior"; //obrabiany tekst
+        int length = message.length(); //długość tekstu - przyda się kilka razy
+
+        //Odpowiedzialność reszty metody main ograniczono do wydrukowania wyszukanych substringów
         System.out.println(message  + "\n");
         System.out.println("Cztery pierwsze znaki: " + subString(message, 0, 4));
         System.out.println("Pięć ostatnich znaków: " + subString(message, length-5, length));
-        int whereToIs = 0;
-        String text;
-        do {
-            text = subString(message, whereToIs, whereToIs+2);
-            whereToIs++;
-        }
-        while(!text.equals("To"));
-        System.out.println("Od \"To\" do końca: " + subString(message, whereToIs-1, length));
+        System.out.println("Od \"To\" do końca: " + subString(message, whereToIs(message, "To"), length));
         System.out.println("Znaki od 3 do 8: " + subString(message, 3-1, 8));
     }
 
@@ -22,5 +17,18 @@ public class Main {
         String textSub = "";
         for(int i = 0; i < end - start; i++) textSub = textSub + textArray[i + start];
         return textSub;
+    }
+
+    public static int whereToIs(String text, String partText) {
+        //metoda znajduje partText w text i zwraca pozycję jego początku
+        int whereToIs = -1;
+        String currentText;
+        do {
+            whereToIs++;
+            currentText = subString(text, whereToIs, whereToIs+2);
+
+        }
+        while(!currentText.equals(partText));
+        return whereToIs;
     }
 }
