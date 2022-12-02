f698b54e24f50249f650ad7ea3c8c212c3467b93
Aspink <aspink1@wp.pl>
2022-12-02 21:38:52 +0100

    dodano import java.util, zdefiniowano ArrayList, dodano tablice z zestawem danych

--- Zadanie 002 dla poczatkujacych/src/Main.java
+++ Zadanie 002 dla poczatkujacych/src/Main.java
@@ -1,8 +1,14 @@
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-    public List filtered(List unfiltered, int repeats) {
+    int n = 2;
+    int[] tabOfInt = {1,2,3,1,2,1,2,3};
+    ArrayList<int> listOfInt = new ArrayList<int>;
+    public ArrayList<int> filtered(ArrayList<int> unfiltered, int repeats) {
 
     }
 }
