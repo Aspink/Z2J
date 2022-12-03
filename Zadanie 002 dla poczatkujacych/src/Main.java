7c39bdd252e31f50be5ff652cc7ef6ff0f56c07d
Aspink <aspink1@wp.pl>
2022-12-03 21:54:34 +0100

    Dodano konwersje tablicy Integer na ArrayList<Integer>

--- Zadanie 002 dla poczatkujacych/src/Main.java
+++ Zadanie 002 dla poczatkujacych/src/Main.java
@@ -1,14 +1,23 @@
 import java.util.*;
-
+import java.lang.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-    int n = 2;
-    int[] tabOfInt = {1,2,3,1,2,1,2,3};
-    ArrayList<int> listOfInt = new ArrayList<int>;
-    public ArrayList<int> filtered(ArrayList<int> unfiltered, int repeats) {
 
+        int n = 2;
+        Integer[] tabOfInt = new Integer[]{1, 2, 3, 1, 2, 1, 2, 3};
+
+        int tabLength = tabOfInt.length;
+        ArrayList<Integer> listOfInt = Arrays.asList(tabOfInt);
+        printList(ListOfInt);
     }
+    private static void printList(ArrayList<Integer> listInt) {
+        for(int i = 0; i < listInt.size; i++) {
+            System.out.print(listInt.get(i));
+        }
+    }
+    /*public ArrayList<int> filtered(ArrayList<int> unfiltered, int repeats) {
+
+    }*/
 }
