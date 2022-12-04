fc7f4e4af7fcf468e0d1d333124b91f5cb1ca2d0
Aspink <aspink1@wp.pl>
2022-12-04 22:04:50 +0100

    Usowanie bledow

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int n = 2;
+        Integer[] tabOfInt = new Integer[]{1, 2, 3, 1, 2, 1, 2, 3};
+
+        int tabLength = tabOfInt.length;
+        List<Integer> listOfInt = Arrays.asList(tabOfInt);
+        printList(listOfInt);
     }
+    public static void printList(List<Integer> listInt) {
+        for(int i = 0; i < listInt.size(); i++) {
+            System.out.print(listInt.get(i));
+        }
+    }
+    /*public ArrayList<int> filtered(ArrayList<int> unfiltered, int repeats) {
+
+    }*/
 }
