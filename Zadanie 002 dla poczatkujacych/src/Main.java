6685b1247d715b7c4216ce88677292cf2d9bad98
Aspink <aspink1@wp.pl>
2022-12-05 11:32:19 +0100

    uproszczenie definiowania listy i jej wydruku

---
+++ Zadanie 002 dla poczatkujacych/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int n1 = 2;
+        ArrayList<Integer> listOfInt1 = new ArrayList<>(Arrays.asList(1, 2, 3, 1, 2, 1, 2, 3));
+        System.out.println(listOfInt1);
+
+    }
+
+    /*public ArrayList<int> filtered(ArrayList<int> unfiltered, int repeats) {
+
+    }*/
+}
