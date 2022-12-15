25036482186e4aa440d2436d58ce95514aba311f
Aspink <aspink1@wp.pl>
2022-12-15 21:41:13 +0100

    Some cleaning, removed some unnecessary outputs.

--- Zadanie 002 dla poczatkujacych/src/Main.java
+++ Zadanie 002 dla poczatkujacych/src/Main.java
@@ -6,22 +6,18 @@ public class Main {
     public static void main(String[] args) {
         Integer n1 = 2;
         ArrayList<Integer> listOfInt1 = new ArrayList<>(Arrays.asList(1, 2, 3, 1, 2, 1, 2, 3));
-        System.out.println("Pierwotna lista: " + listOfInt1 + ", N = " + n1);
+        System.out.println("\nPierwotna lista: " + listOfInt1 + ", N = " + n1);
         System.out.println("Lista zwrócona przez \"filter\": " + filter(listOfInt1, n1));
-        System.out.println("Pierwotna lista po wywołaniu  \"filter\": " + listOfInt1 +"\n");
 
         Integer n2 = 1;
         ArrayList<Integer> listOfInt2 = new ArrayList<>(Arrays.asList(20,37,20,21));
-        System.out.println("Pierwotna lista: " + listOfInt2 + ", N = " + n2);
-
+        System.out.println("\nPierwotna lista: " + listOfInt2 + ", N = " + n2);
         System.out.println("Lista zwrócona przez \"filter\": " + filter(listOfInt2, n2));
-        System.out.println("Pierwotna lista po wywołaniu  \"filter\": " + listOfInt2 +"\n");
 
         Integer n3 = 4;
         ArrayList<Integer> listOfInt3 = new ArrayList<>(Arrays.asList(5,12,5,15,124,15,12,5,124,5,12,15,5,124,124,15,12,15,12,124,5));
-        System.out.println("Pierwotna lista: " + listOfInt3 + ", N = " + n3);
+        System.out.println("\nPierwotna lista: " + listOfInt3 + ", N = " + n3);
         System.out.println("Lista zwrócona przez \"filter\": " + filter(listOfInt3, n3));
-        System.out.println("Pierwotna lista po wywołaniu  \"filter\": " + listOfInt3 +"\n");
     }
 
     public static ArrayList<Integer> filter(ArrayList<Integer> unfiltered, Integer maxRepeats) {
